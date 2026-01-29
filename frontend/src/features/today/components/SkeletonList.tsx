9f3aed6c635e7eefb3ac81ea8f2e4dc041f4fe92
nancy-en-vacance <nancyenvacance@gmail.com>
2026-01-29 19:48:44 +0530

    Delete frontend directory

--- frontend/src/features/today/components/SkeletonList.tsx
+++
@@ -1,9 +0,0 @@
-export default function SkeletonList() {
-  return (
-    <div className="list">
-      {Array.from({ length: 6 }).map((_, i) => (
-        <div className="skeleton" key={i} />
-      ))}
-    </div>
-  );
-}
